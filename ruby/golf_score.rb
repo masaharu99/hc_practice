4a1a58914e1cfeb0c9376fc61c5450348a4df5d0
masaharu <uchiyama.masaharu999@gmail.com>
2024-02-06 11:55:16 +0900

    規定打数3, 4, 5のロジックを一つに統一する

--- ruby/golf_score.rb
+++ ruby/golf_score.rb
@@ -1,82 +1,33 @@
-# 規定打数が3の時のスコアを計算する
-#
-# @params [Integer] player_strokes プレイヤーの打数
-# @return [String] スコア結果
-def calculate_when_par_is_3(player_strokes)
-  case player_strokes
+# 規定打数
+pars = gets.chomp.split(',').map { |par| par.to_i }
+# プレイヤーの打数
+player_strokes =  gets.split(',').map { |stroke| stroke.to_i }
+
+scores = []
+pars.length.times do |i|
+  # プレイヤーの打数が1 かつ 規定打数が3,4のどちらか
+  if player_strokes[i] == 1 && (pars[i] == 3 || pars[i] == 4)
+    scores << 'ホールインワン'
+    next
+  end
+
+  # プレイヤーの打数 - 規定打数
+  score = player_strokes[i] - pars[i]
+  case score
+  when 0
+    scores << 'パー'
+  when -1
+    scores << 'バーディ'
+  when -2
+    scores << 'イーグル'
+  when -3
+    scores << 'アルバトロス'
+  when -4
+    scores << 'コンドル'
   when 1
-    'ホールインワン'
-  when 2
-    'バーディ'
-  when 3
-    'パー'
-  when 4
-    'ボギー'
-  else
-    "#{player_strokes - 3}ボギー"
+    scores << 'ボギー'
+  when (2..)
+    scores << "#{score}ボギー"
   end
 end
-
-# 規定打数が4の時のスコアを計算する
-#
-# @params [Integer] player_strokes プレイヤーの打数
-# @return [String] スコア結果
-def calculate_when_par_is_4(player_strokes)
-  case player_strokes
-  when 1
-    'ホールインワン'
-  when 2
-    'イーグル'
-  when 3
-    'バーディ'
-  when 4
-    'パー'
-  when 5
-    'ボギー'
-  else
-    "#{player_strokes - 4}ボギー"
-  end
-end
-
-# 規定打数が5の時のスコアを計算する
-#
-# @params [Integer] player_strokes プレイヤーの打数
-# @return [String] スコア結果
-def calculate_when_par_is_5(player_strokes)
-  case player_strokes
-  when 1
-    'コンドル'
-  when 2
-    'アルバトロス'
-  when 3
-    'イーグル'
-  when 4
-    'バーディ'
-  when 5
-    'パー'
-  when 6
-    'ボギー'
-  else
-    "#{player_strokes - 5}ボギー"
-  end
-end
-
-def main
-  pars = gets.chomp.split(',')
-  player_strokes =  gets.split(',')
-
-  scores = []
-  pars.length.times do |i|
-    case pars[i].to_i
-    when 3
-      scores << calculate_when_par_is_3(player_strokes[i].to_i)
-    when 4
-      scores << calculate_when_par_is_4(player_strokes[i].to_i)
-    when 5
-      scores << calculate_when_par_is_5(player_strokes[i].to_i)
-    end
-  end
-  puts scores.join(',')
-end
-
-main
+puts scores.join(',')
